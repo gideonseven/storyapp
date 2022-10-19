21a6c782d47cfbd43f0b2e93897bc3c7953c1761
Gideon <gideon@cicil.co.id>
2022-10-20 00:43:30 +0700

    add style

---
+++ app/src/main/java/com/don/storyapp/util/CustomTextInputEditText.kt
@@ -0,0 +1,122 @@
+package com.don.storyapp.util
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+import androidx.core.content.ContextCompat
+import com.don.storyapp.R
+import com.google.android.material.textfield.TextInputEditText
+
+
+/**
+ * Created by gideon on 19 October 2022
+ * gideon@cicil.co.id
+ * https://www.cicil.co.id/
+ */
+
+class CustomTextInputEditText : TextInputEditText, OnTouchListener {
+
+    private lateinit var clearButtonImage: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+    private fun init() {
+        // Menginisialisasi gambar clear button
+        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+
+        // Menambahkan aksi kepada clear button
+        setOnTouchListener(this)
+
+        // Menambahkan aksi ketika ada perubahan text akan memunculkan clear button
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Do nothing.
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) showClearButton() else hideClearButton()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
+    }
+
+    // Menampilkan clear button
+    private fun showClearButton() {
+        setButtonDrawables(endOfTheText = clearButtonImage)
+    }
+
+    //Mengkonfigurasi button
+    private fun setButtonDrawables(startOfTheText: Drawable? = null, topOfTheText:Drawable? = null, endOfTheText:Drawable? = null, bottomOfTheText: Drawable? = null){
+        // Sets the Drawables (if any) to appear to the left of,
+        // above, to the right of, and below the text.
+        setCompoundDrawablesWithIntrinsicBounds(startOfTheText, topOfTheText, endOfTheText, bottomOfTheText)
+    }
+
+    // Menghilangkan clear button
+    private fun hideClearButton() {
+        setButtonDrawables()
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        if (compoundDrawables[2] != null) {
+            val clearButtonStart: Float
+            val clearButtonEnd: Float
+            var isClearButtonClicked = false
+
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                clearButtonEnd = (clearButtonImage.intrinsicWidth + paddingStart).toFloat()
+                when {
+                    event.x < clearButtonEnd -> isClearButtonClicked = true
+                }
+            } else {
+                clearButtonStart = (width - paddingEnd - clearButtonImage.intrinsicWidth).toFloat()
+                when {
+                    event.x > clearButtonStart -> isClearButtonClicked = true
+                }
+            }
+            if (isClearButtonClicked) {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+                        showClearButton()
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+                        when {
+                            text != null -> text?.clear()
+                        }
+                        hideClearButton()
+                        return true
+                    }
+                    else -> return false
+                }
+            } else return false
+        }
+        return false
+    }
+}
