429dde519b84a4658fe10d42ffa12febd76322a3
Gideon <gideon@cicil.co.id>
2022-10-29 09:00:17 +0700

    add image

--- app/src/main/java/com/don/storyApp/util/BindingAdapter.kt
+++ app/src/main/java/com/don/storyApp/util/BindingAdapter.kt
@@ -1,10 +1,27 @@
 package com.don.storyApp.util
 
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.don.storyApp.R
+
 
 /**
  * Created by gideon on 29 October 2022
  * gideon@cicil.co.id
  * https://www.cicil.co.id/
  */
-class BindingAdapter {
+@BindingAdapter("imageSrcFromUrl")
+fun ImageView.loadImageFromUrl(
+    imageUrl: String?
+) {
+    imageUrl?.let {
+        Glide
+            .with(this.context)
+            .load(imageUrl)
+            .centerCrop()
+            .placeholder(R.drawable.ic_placeholder_square)
+            .into(this)
+    }
 }
